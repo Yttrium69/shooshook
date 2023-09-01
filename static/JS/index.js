66828a6954ee842304d419e36f91bb562811b173
Yttrium69 <dmswldmsgp00@naver.com>
2023-09-01 18:22:20 +0900

    GOGOGO

--- static/JS/index.js
+++ static/JS/index.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
     const table = $('.sect_table');
     let initialHeight = 0;
 
-    // resizer.on('mousedown', function(e) {
-    //     initialHeight = e.clientY - table.height();
+    resizer.on('mousedown', function(e) {
+        initialHeight = e.clientY - table.height();
 
-    //     $(document).on('mousemove', resize);
-    //     $(document).on('mouseup', stopResize);
-    // });
+        $(document).on('mousemove', resize_by_mouse);
+        $(document).on('mouseup', stopResize);
+    });
 
     resizer.on('touchstart', function(e) {
         initialHeight = e.originalEvent.touches[0].clientY - table.height();
@@ -25,16 +25,24 @@ $(document).ready(function() {
         $(document).on('touchend', stopResize);
     });
 
+    function resize_by_mouse(e){
+        const new_height = e.clientY;
+        if (new_height >= 50 && new_height<=500) { // You can adjust this minimum height
+            table.height(new_height);
+        }
+    }
+
     function resize(e) {
-        alert(Touch.clientX);
-        const newHeight = e.clientY - initialHeight;
-        if (newHeight >= 100) { // You can adjust this minimum height
+        const newHeight = e.originalEvent.touches[0].clientY - initialHeight;
+        if (newHeight >= 50 && newHeight<=500) { // You can adjust this minimum height
             table.height(newHeight);
         }
     }
 
     function stopResize() {
-        $(document).off('mousemove', resize);
+        $(document).off('mousemove', resize_by_mouse);
         $(document).off('mouseup', stopResize);
+        $(document).off('touchmove', resize);
+        $(document).off('touchend', stopResize);
     }
 });
